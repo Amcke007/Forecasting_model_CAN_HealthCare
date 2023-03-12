e08168394aaaa59840ac304ef67a06c14dc82b81
Alicia Mckeough <aliciamckeough@Alicias-Air.ht.home>
2023-03-12 18:09:15 -0400

    include the headers to csv

--- modify_data/modify_data.rb
+++ modify_data/modify_data.rb
@@ -1,6 +1,9 @@
 require "csv"
 
-lines = IO.readlines('raw_data.csv').map do |line|
+file = IO.readlines('raw_data.csv')
+headers = file.to_a[0]
+
+lines = file.map do |line|
   line.gsub!('"','') # remove quotes
   line_split = line.split(',')
   next if line_split[0].split('-')[0].to_i < 1993 # only continue if the year is greater than 1993
@@ -10,6 +13,7 @@ lines = IO.readlines('raw_data.csv').map do |line|
 end
 
 lines = lines.compact # remove nulls results
+lines.unshift(headers) # add the headers
 
 File.open('processed_data.csv', 'w') do |file|
   file.puts lines
